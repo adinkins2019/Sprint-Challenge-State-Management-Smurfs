8ef382cb71f64802f5a92ae7f37f1f816d5834ad
adinkins2019 <afdinki@gmail.com>
2019-10-26 14:45:57 -0400

    added AddSmurfForm component

---
+++ smurfs/src/components/AddSmurfForm.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function AddSmurfForm(){
+    const handleChange = e => {
+
+    }
+    const handleSubmit = e => {
+
+    }
+
+    return (
+        <form>
+            <input name="addsmurf" />
+            <button>Add New Smurf</button>
+        </form>
+    )
+}
