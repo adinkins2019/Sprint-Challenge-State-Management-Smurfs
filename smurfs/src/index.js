30adfaed44648267eb0f880355a13a0bd402e758
adinkins2019 <afdinki@gmail.com>
2019-10-26 14:44:49 -0400

    Updated main index.js file

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import {createStore, combineReducers, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import {Provider} from 'react-redux'
+import {getSmurfReducer} from './reducers/index'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const rootReducer = combineReducers({
+    smurf: getSmurfReducer
+})
+const store = createStore(rootReducer, applyMiddleware(thunk))
+ReactDOM.render(
+    <Provider store={store}>
+    <App />
+    </Provider>, document.getElementById("root"));
